8ceb5034a1f24e0bd48ee487125a476d38cc6906
JoaoNuno96 <joaonuno1996@hotmail.com>
2025-12-13 17:13:02 +0000

    Jogo da Velha - Finalizado

--- Section 23 - Game Jogo da Velha/gameVelha.cpp
+++ Section 23 - Game Jogo da Velha/gameVelha.cpp
@@ -10,6 +10,8 @@ void mainMenu();
 
 void gameExit();
 
+void boardPrint(char tab[3][3]);
+
 void clearWindow()
 {
 	system("clear");
@@ -57,6 +59,67 @@ void boardGenerate(char tab[3][3])
 	}
 }
 
+void checkPosition(char board[3][3], int *line, int *column, string playerName)
+{
+	while(board[*line][*column] != '-')
+	{
+		clearWindow();
+		boardPrint(board);
+		cout << "Posição impossível! Tente outra!\n";
+		cout << playerName << " - Digite o número da sua linha: ";
+		cin >> *line;
+		cout << playerName << " - Digite o número da sua coluna: ";
+		cin >> *column;
+	}
+}
+
+int checkIfAnyPlayerWin(char board[3][3], int state)
+{
+	int i;
+	
+	//CHECK HORIZONTAL
+	for(i = 0; i < 3; i++)
+	{
+		if(board[i][0] == 'X' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+		{
+			return 1;
+		}
+		else if(board[i][0] == 'O' && board[i][0] == board[i][1] && board[i][1] == board[i][2])
+		{
+			return 2;
+		}
+	}
+	
+	i = 0;
+	
+	//CHECK VERTICAL
+	
+	for(i = 0; i < 3; i++)
+	{
+		if(board[0][i] == 'X' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
+		{
+			return 1;
+		}
+		else if(board[0][i] == 'O' && board[0][i] == board[1][i] && board[1][i] == board[2][i])
+		{
+			return 2;
+		}
+	}
+	
+	
+	//CHECK CROSS'S
+	if(board[0][0] == 'X' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+	{
+		return 1;
+	}
+	if(board[0][0] == 'O' && board[0][0] == board[1][1] && board[1][1] == board[2][2])
+	{
+		return 2;	
+	}
+	
+	return 0;
+}
+
 void boardPrint(char tab[3][3])
 {
 	int i,j;
@@ -70,21 +133,117 @@ void boardPrint(char tab[3][3])
 	}
 }
 
-void playGame()
+
+void playGame(string playerOneName,string playerTwoName, int playerOneScore, int playerTwoScore)
 {
 	//GENERAL VARIABLES
-	 char board[3][3];
+	char board[3][3];
+	int line,column;
+	int numberOfPlays = 0;
+	int gameState = 1;
+	int gameFinish = 0;
+	int rebootOrExit = 0;
+	string currentPlayer = "";
 	 
-	 boardGenerate(board);
-	 boardPrint(board);
+	boardGenerate(board);
+	 
+
+	
+	currentPlayer = playerOneName;
+	 
+	 while (numberOfPlays < 9)
+	{
+		clearWindow();
+		boardPrint(board);
+		
+		cout << "Round#" << numberOfPlays << "\n";
+		
+		cout << currentPlayer << " - Digite o número da sua linha: ";
+		cin >> line;
+		cout << currentPlayer << " - Digite o número da sua coluna: ";
+		cin >> column;
+		
+		checkPosition(board,&line,&column,currentPlayer);
+		
+		if(currentPlayer == playerOneName)
+		{
+			board[line][column] = 'X';
+			currentPlayer = playerTwoName;
+		}
+		else
+		{
+			board[line][column] = 'O';
+			currentPlayer = playerOneName;
+		}
+		
+		numberOfPlays++;
+		
+		//CHECK BOARD IF ANY PLAYER WIN
+		
+		gameFinish = checkIfAnyPlayerWin(board,gameState);
+		
+		if(gameFinish != 0)
+		{
+			break;
+		}
+		else
+		{
+			continue;
+		}
+	}
+	
+	if(gameFinish == 1)
+	{
+		clearWindow();
+		boardPrint(board);
+		playerOneScore++;
+		cout << playerOneName <<  ", venceu o jogo!";
+	}
+	
+	if(gameFinish == 2)
+	{
+		clearWindow();
+		boardPrint(board);
+		playerTwoScore++;
+		cout << playerTwoName << ", venceu o jogo!";
+	}
+	
+	cout << "Fim do jogo.\n";
+	cout << "Pontuação: \n";
+	cout << playerOneName << ": " << playerOneScore << "\n";
+	cout << playerTwoName << ": " << playerTwoScore << "\n";
+	cout << "Deseja reiniciar?\n";
+	cout << "1 - Sim\n";
+	cout << "2 - Não\n";
+	
+	cin >> rebootOrExit;
+	
+	
+	if(rebootOrExit == 1)
+	{
+		playGame(playerOneName,playerTwoName,playerOneScore,playerTwoScore);
+	}
+	else if(rebootOrExit == 2)
+	{
+		gameExit();
+	}
+	
+	 
 }
 
 void choseOption(int number)
 {
+	string playerOneName = "";
+	string playerTwoName = "";
+	
 	switch(number)
 	{
 		case 1:
-			playGame();
+			cout << "Digite nome Jogador 1: ";
+			cin >> playerOneName;
+			cout << "Digite nome Jogador 2: ";
+			cin >> playerTwoName;
+			playGame(playerOneName,playerTwoName,0,0);
 		break;
 	
 		case 2:
@@ -110,7 +269,7 @@ void mainMenu()
 		cout << "1 - Jogar\n";
 		cout << "2 - Informações do Jogo\n";
 		cout << "3 - Desligar\n";
-		
+		cout << "Escolha uma opção: ";
 		cin >> option;
 		
 		choseOption(option);
